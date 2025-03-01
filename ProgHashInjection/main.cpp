8e30bd64cf88cacea460e25af79b28d56a7e69dc
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-01 15:25:48 -0800

    Implemented one-to-one function that hashes the 'name' into an unique array index

--- ProgHashInjection/main.cpp
+++ ProgHashInjection/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <array>
+#include <string>
 #include "Person.h"
 
-
+void hash(std::array<std::array<std::string, 3>, 7> mainArray, std::array<std::string, 3> personArray) {
+    mainArray[personArray[0].length() - 1] = personArray;
+}
 
 int main() {
     std::cout << "Eisig Liang - 2 Mar. 2025\n";
 
-
+    std::array<std::array<std::string, 3>, 7> personArray;
 }
