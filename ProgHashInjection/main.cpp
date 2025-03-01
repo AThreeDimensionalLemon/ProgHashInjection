bb80a71b6944f5189b0783db25d57405c521d992
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-01 15:47:04 -0800

    Implemented hash funcion that determines the proper index based off the name

--- ProgHashInjection/main.cpp
+++ ProgHashInjection/main.cpp
@@ -5,8 +5,19 @@
 
 std::array<std::array<std::string, 3>, 7> personArray;
 
-void oneToOneFunction(std::array<std::string, 3> person) {
+static void oneToOneFunction(std::array<std::string, 3> person) {
     personArray[person[0].length() - 1] = person;
+}
+
+static int hashFunction(std::string name) {
+
+    for (int i = 0; i < 7; i++) {
+
+        if (personArray[i][0] == name)
+            return i + 1;
+    }
+
+    return 0;
 }
 
 int main() {
@@ -15,7 +26,6 @@ int main() {
     std::array<std::string, 7> Name{ "J", "Bo", "Tuy", "Alta", "Kobet", "Teddie", "Sueanna" };
     std::array<std::string, 7> DOB{ "1/5/2001","2/6/2002","3/7/2003","4/8/2004","5/9/2005","6/10/2006" };
     std::array<std::string, 7> FavDessert{ "Cheese Cake", "Vanilla Ice Cream", "Fruit Salad", "Chocolate pudding", "Orange Jell-O", "Apply Pie", "Tres Leches Cake" };
-    
 
     for (int i = 0; i < 7; i++) {
         std::array<std::string, 3> person{ Name[i], DOB[i], FavDessert[i] };
