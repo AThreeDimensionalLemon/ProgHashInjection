988be45ddca588d16a438276a29fb97b13a39191
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-01 15:42:30 -0800

    Fixed issue where one-to-one function doesn't work

--- ProgHashInjection/main.cpp
+++ ProgHashInjection/main.cpp
@@ -3,12 +3,22 @@
 #include <string>
 #include "Person.h"
 
-void hash(std::array<std::array<std::string, 3>, 7> mainArray, std::array<std::string, 3> personArray) {
-    mainArray[personArray[0].length() - 1] = personArray;
+std::array<std::array<std::string, 3>, 7> personArray;
+
+void oneToOneFunction(std::array<std::string, 3> person) {
+    personArray[person[0].length() - 1] = person;
 }
 
 int main() {
     std::cout << "Eisig Liang - 2 Mar. 2025\n";
 
-    std::array<std::array<std::string, 3>, 7> personArray;
+    std::array<std::string, 7> Name{ "J", "Bo", "Tuy", "Alta", "Kobet", "Teddie", "Sueanna" };
+    std::array<std::string, 7> DOB{ "1/5/2001","2/6/2002","3/7/2003","4/8/2004","5/9/2005","6/10/2006" };
+    std::array<std::string, 7> FavDessert{ "Cheese Cake", "Vanilla Ice Cream", "Fruit Salad", "Chocolate pudding", "Orange Jell-O", "Apply Pie", "Tres Leches Cake" };
+    
+
+    for (int i = 0; i < 7; i++) {
+        std::array<std::string, 3> person{ Name[i], DOB[i], FavDessert[i] };
+        oneToOneFunction(person);
+    }
 }
