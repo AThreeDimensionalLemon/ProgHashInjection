7c03485add1fe458a4affa17dd15d639ed0d28b4
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-03-01 16:03:09 -0800

    Implemented `PutData`

--- ProgHashInjection/main.cpp
+++ ProgHashInjection/main.cpp
@@ -6,18 +6,23 @@
 std::array<std::array<std::string, 3>, 7> personArray;
 
 static void oneToOneFunction(std::array<std::string, 3> person) {
+
+    //I'm not sure if "Write the one-to-one function that hashes the 'name' into an unique array index." is a directive or a summary of what I'm supposed to be doing.
+    //For the overall program, I've written it assuming the latter, effectively making this function pointless.
+    //This function is a result of assuming the former, and will be kept in case that was the correct assumption.
+
     personArray[person[0].length() - 1] = person;
 }
 
 static int hashFunction(std::string name) {
+    return name.length() - 1;
+}
 
-    for (int i = 0; i < 7; i++) {
-
-        if (personArray[i][0] == name)
-            return i + 1;
-    }
-
-    return 0;
+static void PutData(std::string inName, std::string favDessert, std::string DOB) {
+    std::array<std::string, 3>* person = &personArray[hashFunction(inName)];
+    (*person)[0] = inName;
+    (*person)[1] = favDessert;
+    (*person)[2] = DOB;
 }
 
 int main() {
@@ -27,8 +32,8 @@ int main() {
     std::array<std::string, 7> DOB{ "1/5/2001","2/6/2002","3/7/2003","4/8/2004","5/9/2005","6/10/2006" };
     std::array<std::string, 7> FavDessert{ "Cheese Cake", "Vanilla Ice Cream", "Fruit Salad", "Chocolate pudding", "Orange Jell-O", "Apply Pie", "Tres Leches Cake" };
 
-    for (int i = 0; i < 7; i++) {
-        std::array<std::string, 3> person{ Name[i], DOB[i], FavDessert[i] };
-        oneToOneFunction(person);
-    }
+    for (int i = 0; i < 7; i++)
+        PutData(Name[i], FavDessert[i], DOB[i]);
+
+
 }
